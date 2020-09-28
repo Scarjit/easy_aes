15b8e94e2c79695dc39f76627a3d9a9ad1063a7e
Ferdinand Linnenberg <s8felinn@stud.uni-saarland.de>
2020-09-29 01:54:05 +0200

    Updated deps & changes lib to return results, instead of panic

--- src/lib.rs
+++ src/lib.rs
@@ -2,41 +2,38 @@ use aes_gcm_siv::aead::generic_array::GenericArray;
 use aes_gcm_siv::aead::{Aead, NewAead};
 use aes_gcm_siv::Aes256GcmSiv;
 use rand::Rng;
+use std::error::Error;
 
 ///Encrypt given data string, returns ciphertext & nounce
 ///Prepends ciphertext with nounce
-pub fn encrypt(plaintext: &str, aes_key: &str) -> String {
-    //println!("Encrypting using: {}", aes_key);
+///Panics if aes key is not 32 chars long
+pub fn encrypt(plaintext: &str, aes_key: &str) -> Result<String, Box<dyn Error>> {
     assert_eq!(aes_key.len(), 32);
     let mut rng = rand::thread_rng();
     let key = GenericArray::from_slice(aes_key.as_bytes());
     let cipher = Aes256GcmSiv::new(key);
     let n = (0..(96 / 8)).map(|_| rng.gen::<u8>()).collect::<Vec<u8>>();
     let nonce = GenericArray::from_slice(&n); //96bit (12 byte) nounce
-    let cipher_text = cipher
-        .encrypt(nonce, plaintext.as_bytes())
-        .expect(&format!("{}", obfstr::obflocal!("Couldn't encrypt")));
+    let cipher_text = cipher.encrypt(nonce, plaintext.as_bytes())?;
     let h_cipher = hex::encode(cipher_text);
     let h_nonce = hex::encode(n);
-    h_nonce + &h_cipher
+    Ok(h_nonce + &h_cipher)
 }
 
 ///Decrypts given ciphertext using key & nounce, returns plaintext
 ///Reads nounce from first 24 chars of ciphertext
-pub fn decrypt(ciphertext: &str, aes_key: &str) -> String {
-    //println!("Decrypting using: {}", aes_key);
+///Panics if aes key is not 32 chars long
+pub fn decrypt(ciphertext: &str, aes_key: &str) -> Result<String, Box<dyn Error>> {
     assert_eq!(aes_key.len(), 32);
     let nx = &ciphertext[0..24];
     let datan = &ciphertext[24..];
-    let data = hex::decode(datan).expect(&format!("{}", obfstr::obflocal!("Couldn't decode hex")));
-    let n = hex::decode(nx).expect(&format!("{}", obfstr::obflocal!("Couldn't decode hex")));
+    let data = hex::decode(datan)?;
+    let n = hex::decode(nx)?;
     let key = GenericArray::from_slice(aes_key.as_bytes());
     let cipher = Aes256GcmSiv::new(key);
     let nonce = GenericArray::from_slice(&n); //96bit (12 byte) nonce
-    let decrypted = cipher
-        .decrypt(nonce, data.as_slice())
-        .expect(&format!("{}", obfstr::obflocal!("Couldn't decrypt")));
-    String::from_utf8(decrypted).expect(&format!("{}", obfstr::obflocal!("Couldn't get string")))
+    let decrypted = cipher.decrypt(nonce, data.as_slice())?;
+    Ok(String::from_utf8(decrypted)?)
 }
 
 #[cfg(test)]
@@ -54,8 +51,8 @@ mod tests {
                 .take(1024)
                 .collect::<String>();
             let tk = rng.sample_iter(&Alphanumeric).take(32).collect::<String>();
-            let encrypted = encrypt(&tx, &tk);
-            let decrypted = decrypt(&encrypted, &tk);
+            let encrypted = encrypt(&tx, &tk).unwrap();
+            let decrypted = decrypt(&encrypted, &tk).unwrap();
             assert_eq!(tx, decrypted);
         }
     }
